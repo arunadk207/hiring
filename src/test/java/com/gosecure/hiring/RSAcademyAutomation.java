cf77e939ce5c2c7b2e74b34a173285069d89f1f2
consultantaruna207 <consultantaruna207@gmail.com>
2020-08-17 23:55:37 -0400

    RSAcademy Added

---
+++ src/test/java/com/gosecure/hiring/RSAcademyAutomation.java
@@ -0,0 +1,15 @@
+package com.gosecure.hiring;
+
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+class RSAcademyAutomation {
+
+	@Test
+	public void setup() {
+		assertEquals("Hello", "Hello");
+
+	}
+
+}
